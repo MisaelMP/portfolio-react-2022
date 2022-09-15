d57f788084ef30f31ea0a17b6e5902766e3b8f44
Misael  M <misaelmdev@gmail.com>
2022-09-16 00:01:26 +1000

    added tailwind and style-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
@@ -11,7 +10,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="text-4xl mb-10 text-red-400"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
