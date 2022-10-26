750d00d993066b6130e11d78b88cfdb720de8c1f
Misael M <misaelmdev@gmail.com>
2022-10-27 09:23:13 +1100

    feat: added google fonts feat: added global styles and tailwind fonts

--- src/modules/sections/components/Projects.tsx
+++ src/modules/sections/components/Projects.tsx
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
 
 const Projects = () => {
-  <Fragment>
-    <h2>I was thinking Projects you</h2>
-    <h3>(prolegomena)</h3>
-  </Fragment>;
+  return (
+    <Fragment>
+      <h2>I was thinking Contact you</h2>
+      <h3>(prolegomena)</h3>
+    </Fragment>
+  );
 };
+
 
 export default Projects;
