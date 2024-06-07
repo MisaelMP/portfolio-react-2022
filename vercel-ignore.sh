f1e44f0874b1d0f93a7bd4b24cf2cad7442d382e
Misael  M <misaelmdev@gmail.com>
2024-06-07 16:06:06 +1000

    feat: changed script

--- vercel-ignore.sh
+++ vercel-ignore.sh
@@ -1,36 +1,10 @@
-#!/bin/bash
+if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "master"  ]] ; then
+  # Proceed with the build
+    echo "✅ - Build can proceed"
+  exit 1;
 
-# Define branches to ignore
-IGNORED_BRANCHES=("develop" "feature/*")
-
-# Get the current branch name from GITHUB_REF environment variable
-BRANCH=$(echo $GITHUB_REF | sed 's|refs/heads/||')
-
-echo "Current branch: $BRANCH"
-
-# Function to check if the branch matches any pattern in the IGNORED_BRANCHES array
-should_ignore() {
-  for IGNORED_BRANCH in "${IGNORED_BRANCHES[@]}"; do
-    if [[ "$IGNORED_BRANCH" == *"/*" ]]; then
-      # If the pattern is a wildcard, use a glob pattern match
-      if [[ "$BRANCH" == ${IGNORED_BRANCH/\*/}* ]]; then
-        return 0
-      fi
-    else
-      # Otherwise, use exact match
-      if [[ "$BRANCH" == "$IGNORED_BRANCH" ]]; then
-        return 0
-      fi
-    fi
-  done
-  return 1
-}
-
-# Check if the current branch should be ignored
-if should_ignore; then
-  echo "🛑 - Build cancelled"
-  exit 0
 else
-  echo "✅ - Build can proceed"
-  exit 1
+  # Don't build
+  echo "🛑 - Build cancelled"
+  exit 0;
 fi
