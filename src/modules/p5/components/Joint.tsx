e36ef43ea63571eba8ee8cade2ee90f216e0dead
Misael  M <misaelmdev@gmail.com>
2024-06-07 15:55:26 +1000

    feat: deleted dead code

--- src/modules/p5/components/Joint.tsx
+++ src/modules/p5/components/Joint.tsx
@@ -1,44 +1,6 @@
 import { JointParams, PointParams } from '../../../ts/interfaces/blob.interfaces';
 
 import p5Types from 'p5';
-
-// let pointA: PointParams[];
-// let pointB: PointParams[];
-
-// function update(dt = 1){
-//   const diffx = pointA.x - pointB.x;
-//   const diffy = pointA.y - pointB.y;
-//   const mag = window.p5.prototype.sqrt(diffx * diffx + diffy * diffy);
-//   const diffMag = len - mag;
-//   if (mag > 0) {
-//     const invMag = 1 / mag;
-//     const forceX = diffx * invMag * diffMag * strength * 0.5 * dt;
-//     const forceY = diffy * invMag * diffMag * strength * 0.5 * dt;
-//     pointA.addForce(forceX, forceY);
-//     pointB.addForce(-forceX, -forceY);
-//   }
-// };
-
-// function draw(this: any, ctx: p5Types){
-//   ctx.line(this.pointA.x, this.pointA.y, this.pointB.x, this.pointB.y);
-// };
-
-// export const joint = (
-//   pointA: object,
-//   pointB: object,
-//   len: number,
-//   strength: number
-// ): JointParams => {
-//   return {
-//     pointA,
-//     pointB,
-//     len,
-//     strength,
-//     update,
-//     draw,
-//   };
-// };
-
 export class Joint implements JointParams {
   pointA: PointParams;
   pointB: PointParams;
