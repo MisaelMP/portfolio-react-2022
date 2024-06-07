17ab49f054edf8ef61395dbfe0df2d9ebebcdaee
Misael  M <misaelmdev@gmail.com>
2024-06-07 15:11:53 +1000

    feat: added vercel script

---
+++ vercel-ignore.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Define branches to ignore
+IGNORED_BRANCHES=("develop" "feature/*")
+
+# Get the current branch
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+# Check if the current branch should be ignored
+for IGNORED_BRANCH in "${IGNORED_BRANCHES[@]}"; do
+  if [[ "$BRANCH" == $IGNORED_BRANCH ]]; then
+    echo "🛑 - Build cancelled"
+    exit 0;
+  fi
+done
+
+echo "✅ - Build can proceed"
+exit 1;
