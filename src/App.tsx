978c5437b3766c7fc01b6e856029430e63c0ac25
Misael  M <misaelmdev@gmail.com>
2024-06-09 06:56:38 +1000

    feat: added animation and styles for form and other pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
+import { useEffect, useRef } from 'react';
 import { Outlet } from 'react-router-dom';
-import GlobalStyle from './styles/generic/GlobalStyles';
 import NavBar from './modules/layout/components/NavBar';
 import Footer from './modules/layout/components/Footer';
 import Section from './modules/common/components/Section';
@@ -8,20 +8,35 @@ import { useLocation } from 'react-router-dom';
 
 function App(): JSX.Element {
 	const location = useLocation();
+	const headerRef = useRef<HTMLDivElement>(null);
+	const footerRef = useRef<HTMLDivElement>(null);
+	const mainRef = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const headerHeight = headerRef.current ? headerRef.current.offsetHeight : 0;
+		const footerHeight = footerRef.current ? footerRef.current.offsetHeight : 0;
+		const mainHeight = `calc(100vh - ${headerHeight}px - ${footerHeight}px)`;
+		if (mainRef.current) {
+			mainRef.current.style.height = mainHeight;
+		}
+	}, [location]);
 	return (
-		<div className='flex flex-col h-screen justify-start'>
-			<GlobalStyle />
-			<header className='e-header'>
+		<div className='flex flex-col h-screen'>
+			<header className='e-header' ref={headerRef}>
 				<NavBar />
 			</header>
-			<main className='relative h-full w-full' data-canvas>
-				<TransitionGroup className="h-full">
+			<main
+				className='relative h-full w-full overflow-auto scrollbar scrollbar-thumb-gray-500 scrollbar-track-gray-200 '
+				data-canvas
+				ref={mainRef}
+			>
+				<TransitionGroup className='h-full'>
 					<CSSTransition timeout={200} classNames='fade' key={location.key}>
 						<Outlet />
 					</CSSTransition>
 				</TransitionGroup>
 			</main>
-			<footer className='mt-auto z-20'>
+			<footer className='mt-auto z-20' ref={footerRef}>
 				<Section section='footer'>
 					<Footer />
 				</Section>
