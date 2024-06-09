fdbedb9e899a3dcc4bab5524197a65e3e553a6f5
Misael  M <misaelmdev@gmail.com>
2024-06-09 15:30:56 +1000

    feat: commented animation

--- src/modules/interaction/components/Stage.tsx
+++ src/modules/interaction/components/Stage.tsx
@@ -1,129 +1,129 @@
-// Stage.tsx
-import React, { useEffect, useRef } from 'react';
-import * as THREE from 'three';
-import * as C from 'cannon';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import MenuSticky from './MenuSticky';
+// // Stage.tsx
+// import React, { useEffect, useRef } from 'react';
+// import * as THREE from 'three';
+// import * as C from 'cannon';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// import MenuSticky from './MenuSticky';
 
-const distance = 15;
+// const distance = 15;
 
-interface LayoutProps {
-	W: number;
-	H: number;
-}
+// interface LayoutProps {
+// 	W: number;
+// 	H: number;
+// }
 
-interface StageProps {
-	layout: LayoutProps;
-}
+// interface StageProps {
+// 	layout: LayoutProps;
+// }
 
-const Stage: React.FC<StageProps> = ({ layout }) => {
-	const stageRef = useRef<HTMLCanvasElement | null>(null);
-	const worldRef = useRef<C.World>(new C.World());
-	const sceneRef = useRef<THREE.Scene>(new THREE.Scene());
-	const cameraRef = useRef<THREE.OrthographicCamera | null>(null);
-	const rendererRef = useRef<THREE.WebGLRenderer | null>(null);
-	const menuRef = useRef<typeof MenuSticky | null>(null);
+// const Stage: React.FC<StageProps> = ({ layout }) => {
+// 	const stageRef = useRef<HTMLCanvasElement | null>(null);
+// 	const worldRef = useRef<C.World>(new C.World());
+// 	const sceneRef = useRef<THREE.Scene>(new THREE.Scene());
+// 	const cameraRef = useRef<THREE.OrthographicCamera | null>(null);
+// 	const rendererRef = useRef<THREE.WebGLRenderer | null>(null);
+// 	const menuRef = useRef<typeof MenuSticky | null>(null);
 
-	useEffect(() => {
-		const $stage = stageRef.current;
+// 	useEffect(() => {
+// 		const $stage = stageRef.current;
 
-		// Init Physics world
-		const world = worldRef.current;
-		world.gravity.set(0, -50, 0);
+// 		// Init Physics world
+// 		const world = worldRef.current;
+// 		world.gravity.set(0, -50, 0);
 
-		// Init Three components
-		const scene = sceneRef.current;
-		scene.fog = new THREE.Fog(0x312929, -10, 100);
+// 		// Init Three components
+// 		const scene = sceneRef.current;
+// 		scene.fog = new THREE.Fog(0x312929, -10, 100);
 
-		setCamera(layout);
-		setLights();
-		setRender($stage, layout);
-		addObjects();
+// 		setCamera(layout);
+// 		setLights();
+// 		setRender($stage, layout);
+// 		addObjects();
 
-		return () => {
-			window.removeEventListener('resize', onResize);
-		};
-	}, [layout]);
+// 		return () => {
+// 			window.removeEventListener('resize', onResize);
+// 		};
+// 	}, [layout]);
 
-	const onResize = () => {
-		if (!cameraRef.current) return;
+// 	const onResize = () => {
+// 		if (!cameraRef.current) return;
 
-		const { W, H } = layout;
-		cameraRef.current.aspect = W / H;
-		cameraRef.current.top = distance;
-		cameraRef.current.right = distance * cameraRef.current.aspect;
-		cameraRef.current.bottom = -distance;
-		cameraRef.current.left = -distance * cameraRef.current.aspect;
+// 		const { W, H } = layout;
+// 		cameraRef.current.aspect = W / H;
+// 		cameraRef.current.top = distance;
+// 		cameraRef.current.right = distance * cameraRef.current.aspect;
+// 		cameraRef.current.bottom = -distance;
+// 		cameraRef.current.left = -distance * cameraRef.current.aspect;
 
-		cameraRef.current.updateProjectionMatrix();
-		rendererRef.current?.setSize(W, H);
-	};
+// 		cameraRef.current.updateProjectionMatrix();
+// 		rendererRef.current?.setSize(W, H);
+// 	};
 
-	const setCamera = ({ W, H }: LayoutProps) => {
-		const aspect = W / H;
-		cameraRef.current = new THREE.OrthographicCamera(
-			-distance * aspect,
-			distance * aspect,
-			distance,
-			-distance,
-			-10,
-			100
-		);
-		cameraRef.current.position.set(-10, 10, 10);
-		cameraRef.current.lookAt(new THREE.Vector3());
-	};
+// 	const setCamera = ({ W, H }: LayoutProps) => {
+// 		const aspect = W / H;
+// 		cameraRef.current = new THREE.OrthographicCamera(
+// 			-distance * aspect,
+// 			distance * aspect,
+// 			distance,
+// 			-distance,
+// 			-10,
+// 			100
+// 		);
+// 		cameraRef.current.position.set(-10, 10, 10);
+// 		cameraRef.current.lookAt(new THREE.Vector3());
+// 	};
 
-	const setLights = () => {
-		const scene = sceneRef.current;
+// 	const setLights = () => {
+// 		const scene = sceneRef.current;
 
-		const ambient = new THREE.AmbientLight(0xcccccc);
-		scene.add(ambient);
+// 		const ambient = new THREE.AmbientLight(0xcccccc);
+// 		scene.add(ambient);
 
-		const foreLight = new THREE.DirectionalLight(0xffffff, 0.5);
-		foreLight.position.set(5, 5, 20);
-		scene.add(foreLight);
+// 		const foreLight = new THREE.DirectionalLight(0xffffff, 0.5);
+// 		foreLight.position.set(5, 5, 20);
+// 		scene.add(foreLight);
 
-		const backLight = new THREE.DirectionalLight(0xffffff, 1);
-		backLight.position.set(-5, -5, -10);
-		scene.add(backLight);
-	};
+// 		const backLight = new THREE.DirectionalLight(0xffffff, 1);
+// 		backLight.position.set(-5, -5, -10);
+// 		scene.add(backLight);
+// 	};
 
-	const setRender = ($stage: HTMLCanvasElement | null, { W, H }: LayoutProps) => {
-		if (!$stage) return;
+// 	const setRender = ($stage: HTMLCanvasElement | null, { W, H }: LayoutProps) => {
+// 		if (!$stage) return;
 
-		rendererRef.current = new THREE.WebGLRenderer({
-			antialias: true,
-			canvas: $stage,
-		});
+// 		rendererRef.current = new THREE.WebGLRenderer({
+// 			antialias: true,
+// 			canvas: $stage,
+// 		});
 
-		rendererRef.current.setClearColor(0x312929);
-		rendererRef.current.setSize(W, H);
-		rendererRef.current.setPixelRatio(window.devicePixelRatio);
-		rendererRef.current.setAnimationLoop(() => draw());
-	};
+// 		rendererRef.current.setClearColor(0x312929);
+// 		rendererRef.current.setSize(W, H);
+// 		rendererRef.current.setPixelRatio(window.devicePixelRatio);
+// 		rendererRef.current.setAnimationLoop(() => draw());
+// 	};
 
-	const addObjects = () => {
-		const scene = sceneRef.current;
-		const world = worldRef.current;
-		const camera = cameraRef.current;
+// 	const addObjects = () => {
+// 		const scene = sceneRef.current;
+// 		const world = worldRef.current;
+// 		const camera = cameraRef.current;
 
-		if (camera) {
-			return <MenuSticky ref={menuRef} scene={scene} world={world} camera={camera} />;
-		}
-	};
+// 		if (camera) {
+// 			return <MenuSticky ref={menuRef} scene={scene} world={world} camera={camera} />;
+// 		}
+// 	};
 
-	const draw = () => {
-		updatePhysics();
-		rendererRef.current?.render(sceneRef.current, cameraRef.current!);
-	};
+// 	const draw = () => {
+// 		updatePhysics();
+// 		rendererRef.current?.render(sceneRef.current, cameraRef.current!);
+// 	};
 
-	const updatePhysics = () => {
-		menuRef.current?.update();
+// 	const updatePhysics = () => {
+// 		menuRef.current?.update();
 
-		worldRef.current.step(1 / 60);
-	};
+// 		worldRef.current.step(1 / 60);
+// 	};
 
-	return <canvas id='stage' ref={stageRef} />;
-};
+// 	return <canvas id='stage' ref={stageRef} />;
+// };
 
-export default Stage;
+// export default Stage;
--- src/modules/interaction/components/TypoLandPage.tsx
+++ src/modules/interaction/components/TypoLandPage.tsx
@@ -1,21 +1,21 @@
-// App.tsx
-import React, { useState, useCallback } from 'react';
-import Layout from './Layout';
-import Stage from './Stage';
+// // App.tsx
+// import React, { useState, useCallback } from 'react';
+// import Layout from './Layout';
+// import Stage from './Stage';
 
-const TypoLandPage: React.FC = () => {
-	const [layout, setLayout] = useState({ W: 0, H: 0 });
+// const TypoLandPage: React.FC = () => {
+// 	const [layout, setLayout] = useState({ W: 0, H: 0 });
 
-	const onResize = useCallback(({ W, H }) => {
-		setLayout({ W, H });
-	}, []);
+// 	const onResize = useCallback(({ W, H }) => {
+// 		setLayout({ W, H });
+// 	}, []);
 
-	return (
-		<div>
-			<Layout onResize={onResize} />
-			<Stage layout={layout} />
-		</div>
-	);
-};
+// 	return (
+// 		<div>
+// 			<Layout onResize={onResize} />
+// 			<Stage layout={layout} />
+// 		</div>
+// 	);
+// };
 
-export default TypoLandPage;
+// export default TypoLandPage;
