c743c75c7075728cea5e748f442ef43c9a63846d
Misael  M <misaelmdev@gmail.com>
2024-06-13 15:55:42 +1000

    feat: fixed animation for particles and react dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import NavBar from './modules/layout/components/NavBar';
 import Footer from './modules/layout/components/Footer';
 import Section from './modules/common/components/Section';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import { useLocation } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 
 function App(): JSX.Element {
 	const location = useLocation();
 
-  useEffect(() => {
-    const canvas = document.querySelector('[data-canvas]');
-    if (canvas) {
-      canvas.scrollTop = 0;
-    }
-
-  }, [location])
-  
+	useEffect(() => {
+		const canvas = document.querySelector('[data-canvas]');
+		if (canvas) {
+			canvas.scrollTop = 0;
+		}
+	}, [location]);
 
 	return (
 		<div className='flex flex-col'>
