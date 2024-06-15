446e818f45b13e9d866f3dbb03e1d41c0bed729c
Misael  M <misaelmdev@gmail.com>
2024-06-15 18:23:20 +1000

    feat: try deploy again

--- vercel-ignore.sh
+++ vercel-ignore.sh
@@ -1,3 +1,5 @@
+VERCEL_GIT_COMMIT_REF="$(echo -e "${VERCEL_GIT_COMMIT_REF}" | tr -d '[:space:]')"
+
 echo "VERCEL_GIT_COMMIT_REF: '$VERCEL_GIT_COMMIT_REF'"
 
 if [ "$VERCEL_GIT_COMMIT_REF" != "master" ] && [ "$VERCEL_GIT_COMMIT_REF" != "development" ]; then
