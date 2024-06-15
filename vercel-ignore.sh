f206a441b4c747f225852c631e8cc00f6dba5b08
Misael  M <misaelmdev@gmail.com>
2024-06-15 18:42:07 +1000

    feat: change vercel bash

--- vercel-ignore.sh
+++ vercel-ignore.sh
@@ -1,6 +1,14 @@
-echo "VERCEL_GIT_COMMIT_REF: '$VERCEL_GIT_COMMIT_REF'"
+#!/bin/bash
 
-if [ "$VERCEL_GIT_COMMIT_REF" != "master" ] && [ "$VERCEL_GIT_COMMIT_REF" != "development" ]; then
+echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+
+if [[ "$VERCEL_GIT_COMMIT_REF" == "development" || "$VERCEL_GIT_COMMIT_REF" == "master"  ]] ; then
+  # Proceed with the build
+    echo "✅ - Build can proceed"
+  exit 1;
+
+else
+  # Don't build
   echo "🛑 - Build cancelled"
-  exit 0
+  exit 0;
 fi
