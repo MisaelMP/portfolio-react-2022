a01cacb0649ac6039dbee0f804312728e00bb086
Misael  M <misaelmdev@gmail.com>
2024-06-15 18:30:36 +1000

    feat: another try

--- vercel-ignore.sh
+++ vercel-ignore.sh
@@ -1,6 +1,6 @@
 echo "VERCEL_GIT_COMMIT_REF: '$VERCEL_GIT_COMMIT_REF'"
 
-if [ "$VERCEL_GIT_COMMIT_REF" != "master" ] || [ "$VERCEL_GIT_COMMIT_REF" != "development" ]; then
+if [ "$VERCEL_GIT_COMMIT_REF" != "master" ] && [ "$VERCEL_GIT_COMMIT_REF" != "development" ]; then
   echo "🛑 - Build cancelled"
   exit 0
 fi
