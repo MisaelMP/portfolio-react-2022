9b2999aeaa89416554ac873ab58aae02d3aadd59
Misael  M <misaelmdev@gmail.com>
2024-06-15 17:40:44 +1000

    feat: changed to vite prefix

--- vercel-ignore.sh
+++ vercel-ignore.sh
@@ -1,6 +1,6 @@
-echo "VERCEL_GIT_COMMIT_REF: '$VERCEL_GIT_COMMIT_REF'"
+echo "VITE_VERCEL_GIT_COMMIT_REF: '$VERCEL_GIT_COMMIT_REF'"
 
-if [ "$VERCEL_GIT_COMMIT_REF" != "master" ] && [ "$VERCEL_GIT_COMMIT_REF" != "development" ]; then
+if [ "$VITE_VERCEL_GIT_COMMIT_REF" != "master" ] && [ "$VITE_VERCEL_GIT_COMMIT_REF" != "development" ]; then
   echo "🛑 - Build cancelled"
   exit 0
 fi
